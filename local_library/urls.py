aa74ba68313c0cc1d608865bfa9de9434293b4e4
Marco <rootslowed@gmail.com>
2021-02-12 11:21:05 -0300

    URL maps to redirect the base URL to catalog application

--- local_library/urls.py
+++ local_library/urls.py
@@ -1,11 +1,12 @@
 # urls.py define os mapeamentos de URL para visualização do site. Mesmo que esse arquivo possa conter todo o código para mapeamento de URL, é mais comum delegar apenas o mapeamento para aplicativos específicos, como será visto mais adiante.
 
 from django.contrib import admin
-from django.conf.urls import include
+from django.conf.urls import include, url
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-
+    path('', RedirectView.as_view(url='/catalog/')),
 ]
