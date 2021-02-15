b249eb10c70e5329fc45ec31f15a279bbf54b2be
Marco <rootslowed@gmail.com>
2021-02-15 13:56:21 -0300

    model for Genre implemented

--- catalog/models.py
+++ catalog/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 from django.urls import reverse
+
+
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter a book genre')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
